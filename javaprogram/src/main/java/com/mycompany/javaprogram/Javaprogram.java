a00243ad88bcd4b6ebd01552679aa51bb62465b4
st10180410 <st10180410@RCLMPOCR2PC39.student.advtech.co.za>
2022-05-13 14:32:46 +0200

    hello

---
+++ javaprogram/src/main/java/com/mycompany/javaprogram/Javaprogram.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.javaprogram;
+
+/**
+ *
+ * @author st10180410
+ */
+public class Javaprogram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
